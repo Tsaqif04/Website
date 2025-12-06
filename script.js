123b3a6376a1eaa5d6b200e57b5be600a8f52dd6
Tsaqif04 <hilmantsaqif11@gmail.com>
2025-12-06 20:56:29 +0700

    update main

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const navMenu = document.getElementById("navMenu")
 hamburgerBtn.addEventListener("click", () => {
     navMenu.classList.toggle("show");
 });
+let screw = document.querySelector('.screw');
+anime({
+    targets: '.screw',
+    rotate: 360,
+    duration: 10000,
+    easing: 'linear',
+    loop: true
+});
